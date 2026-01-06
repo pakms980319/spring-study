424e53c1c65e19e009ec72673ce04a4feecc95aa
pakms980319 <pakms980319@gmail.com>
2026-01-06 11:55:00 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 섹션 3. 스프링 웹 개발 기초 - 9. API

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,45 @@
+package hello.hello_spring.controller;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("data", "hello!!");
+
+		return "hello";
+	}
+
+	@GetMapping("/hello-mvc")
+	public String helloMvc(@RequestParam(name = "name", defaultValue = "Empty") String name,
+	                       Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
+	}
+
+	@GetMapping("/hello-string")
+	@ResponseBody
+	public String helloString(@RequestParam(name = "name", defaultValue = "Empty") String name) {
+		return "hello " + name;
+	}
+
+	@GetMapping("/hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam(name = "name", defaultValue = "Empty") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		return hello;
+	}
+
+	@Data
+	static class Hello {
+		private String name;
+	}
+}
