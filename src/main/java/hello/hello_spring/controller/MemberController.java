ec60283a9fa3f83d5735aaf8b9a04dd40e87cbec
pakms980319 <pakms980319@gmail.com>
2026-01-06 15:39:33 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 섹션 6. 회원 관리 예제 - 웹 MVC 개발 - 19. 회원 웹 기능 (등록, 조회)

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,13 +1,47 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@Slf4j
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
 	private final MemberService service;
 
+	@GetMapping("/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping("/new")
+	public String create(@ModelAttribute MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+
+		service.join(member);
+
+		log.info("member : {}", member);
+
+		return "redirect:/";
+	}
+
+	@GetMapping
+	public String list(Model model) {
+		List<Member> members = service.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+	}
 }
---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,8 @@
+package hello.hello_spring.controller;
+
+import lombok.Data;
+
+@Data
+public class MemberForm {
+	String name;
+}
