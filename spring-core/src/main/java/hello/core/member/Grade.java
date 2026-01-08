cb94f9c0af276d0a683b10ee6c39a90f343fc135
pakms980319 <pakms980319@gmail.com>
2026-01-08 15:07:51 +0900

    스프링 핵심 원리 - 기본편 섹션 3. 스프링 핵심 원리 이해1 - 예제 만들기

---
+++ spring-core/src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,6 @@
+package hello.core.member;
+
+public enum Grade {
+	BASIC, // 일반 회원
+	VIP // VIP
+}
---
+++ spring-core/src/main/java/hello/core/order/Order.java
@@ -0,0 +1,13 @@
+package hello.core.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Order {
+	private Long memberId;
+	private String itemName;
+	private int itemPrice;
+	private int discountPrice;
+}
