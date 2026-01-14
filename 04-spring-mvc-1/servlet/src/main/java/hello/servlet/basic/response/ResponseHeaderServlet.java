ba810077f36abf53649d50aed52ade3fd358eef2
pakms980319 <pakms980319@gmail.com>
2026-01-14 17:34:25 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 섹션 3. 서블릿

---
+++ 04-spring-mvc-1/servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,50 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ResponseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// [status-line]
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		// [response-headers]
+		// response.setHeader("Content-type", "text/plain;charset=utf-8");
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("my-header", "hello");
+
+		// [Header 편의 메서드]
+		content(response);
+		cookie(response);
+		redirect(response);
+
+		PrintWriter writer = response.getWriter();
+		writer.println("ok");
+	}
+
+	private void redirect(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/basic/hello-form.html");
+	}
+
+	private void cookie(HttpServletResponse response) {
+		Cookie cookie = new Cookie("myCookie", "good");
+		cookie.setMaxAge(600);  // 600초
+		response.addCookie(cookie);
+	}
+
+	private void content(HttpServletResponse response) {
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("utf-8");
+	}
+}
