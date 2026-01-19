30a71c10997f3341399628647dabbcb06c8b2396
pakms980319 <pakms980319@gmail.com>
2026-01-19 11:51:31 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 섹션 5. MVC 프레임워크 만들기 - 단순하고 실용적인 컨트롤러 - v4 (34)

---
+++ 04-spring-mvc-1/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,71 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+	private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+	public FrontControllerServletV4() {
+		controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+		controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+		controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+
+		ControllerV4 controller = controllerMap.get(requestURI);
+		if (controller == null) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+
+		Map<String, String> paramMap = createParamMap(request);
+
+		System.out.println("service paramMap : " + paramMap);
+
+		Map<String, Object> model = new HashMap<>();
+		String viewName = controller.process(paramMap, model);
+
+		System.out.println("service viewName = " + viewName);
+
+		MyView view = viewResolver(viewName);
+
+		view.render(model, request, response);
+	}
+
+	private MyView viewResolver(String viewName) {
+		return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+	}
+
+	private Map<String, String> createParamMap(HttpServletRequest request) {
+		Enumeration<String> params = request.getParameterNames();
+		while (params.hasMoreElements()) {
+			String name = params.nextElement();
+			System.out.println("name = " + request.getParameter(name));
+		}
+
+		Map<String, String> paramMap = new HashMap<>();
+		request.getParameterNames()
+		       .asIterator()
+		       .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+
+		System.out.println("createParamMap paramMap : " + paramMap);
+
+		return paramMap;
+	}
+}
