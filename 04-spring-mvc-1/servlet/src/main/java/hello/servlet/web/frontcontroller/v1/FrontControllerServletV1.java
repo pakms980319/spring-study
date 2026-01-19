2f2a5fa84dd73f5bfe3d56244712c7dc16437909
pakms980319 <pakms980319@gmail.com>
2026-01-19 10:21:29 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 섹션 5. MVC 프레임워크 만들기 - View 분리 - V2 (32)

--- 04-spring-mvc-1/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ 04-spring-mvc-1/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -1,6 +1,5 @@
-package hello.servlet.web.frontcontroller;
+package hello.servlet.web.frontcontroller.v1;
 
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
