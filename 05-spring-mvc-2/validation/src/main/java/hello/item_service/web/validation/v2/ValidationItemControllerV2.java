ccf6a9e9edaeb596bcd5f27d85924f9e4a72a466
pakms980319 <pakms980319@gmail.com>
2026-02-03 13:17:16 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 5. 검증1 - Validation - FieldError, ObjectError (47)

--- 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
+++ 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
@@ -88,15 +88,15 @@ public class ValidationItemControllerV2 {
 	                        Model model) {
 		// 검증 로직
 		if (!StringUtils.hasText(item.getItemName())) {
-			bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
+			bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
 		}
 
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-			bindingResult.addError(new FieldError("item", "price", "가격은 1,000원 ~ 1,000,000원까지 허용합니다."));
+			bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000원 ~ 1,000,000원까지 허용합니다."));
 		}
 
 		if (item.getQuantity() == null || item.getQuantity() > 9999) {
-			bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999개까지 허용합니다."));
+			bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999개까지 허용합니다."));
 		}
 
 		// 특정 필드가 아닌 복합 룰 검증
@@ -104,7 +104,7 @@ public class ValidationItemControllerV2 {
 			int resultPrice = item.getPrice() * item.getQuantity();
 
 			if (resultPrice < 10000) {
-				bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+				bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
 			}
 		}
 
