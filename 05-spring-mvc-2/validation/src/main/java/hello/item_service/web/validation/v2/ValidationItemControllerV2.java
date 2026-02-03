b59d191de8ef5e48bed2634ed2451e67104790b3
pakms980319 <pakms980319@gmail.com>
2026-02-03 18:18:37 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 5. 검증1 - Validation - 오류 코드와 메시지 처리2

--- 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
+++ 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
@@ -126,23 +126,20 @@ public class ValidationItemControllerV2 {
 	                        BindingResult bindingResult,
 	                        RedirectAttributes redirectAttributes,
 	                        Model model) {
+		log.info("objectName = {}", bindingResult.getObjectName());
+		log.info("target = {}", bindingResult.getTarget());
+
 		// 검증 로직
 		if (!StringUtils.hasText(item.getItemName())) {
-			bindingResult.addError(
-				new FieldError("item", "itemName", item.getItemName(), false, new String[] {"required.item.itemName"}, null, null)
-			);
+			bindingResult.rejectValue("itemName", "required");
 		}
 
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-			bindingResult.addError(
-				new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null)
-			);
+			bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
 		}
 
 		if (item.getQuantity() == null || item.getQuantity() > 9999) {
-			bindingResult.addError(
-				new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null)
-			);
+			bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
 		}
 
 		// 특정 필드가 아닌 복합 룰 검증
@@ -150,9 +147,7 @@ public class ValidationItemControllerV2 {
 			int resultPrice = item.getPrice() * item.getQuantity();
 
 			if (resultPrice < 10000) {
-				bindingResult.addError(
-					new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{"10000", resultPrice}, null)
-				);
+				bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
 			}
 		}
 
