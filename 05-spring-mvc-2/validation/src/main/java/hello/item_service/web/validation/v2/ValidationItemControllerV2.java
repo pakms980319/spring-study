cf4e89d877a940d4bb2a268621f1e4cc50bedd4d
pakms980319 <pakms980319@gmail.com>
2026-02-04 17:02:06 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 5. 검증1 - Validation - 오류 코드와 메시지 처리5

--- 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
+++ 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/v2/ValidationItemControllerV2.java
@@ -12,6 +12,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -130,9 +131,11 @@ public class ValidationItemControllerV2 {
 		log.info("target = {}", bindingResult.getTarget());
 
 		// 검증 로직
-		if (!StringUtils.hasText(item.getItemName())) {
-			bindingResult.rejectValue("itemName", "required");
-		}
+		// if (!StringUtils.hasText(item.getItemName())) {
+		// 	bindingResult.rejectValue("itemName", "required");
+		// }
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
 
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
 			bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
