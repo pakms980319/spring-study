289c3c73ec5fb0f835e9c60c1b565c29513a0b39
pakms980319 <pakms980319@gmail.com>
2026-02-06 11:52:10 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 6. 검증2 - Bean Validation - Form 전송 객체 분리 - 프로젝트 준비 V4

---
+++ 05-spring-mvc-2/validation/src/main/java/hello/item_service/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,189 @@
+package hello.item_service.web.validation;
+
+import hello.item_service.domain.item.DeliveryCode;
+import hello.item_service.domain.item.Item;
+import hello.item_service.domain.item.ItemRepository;
+import hello.item_service.domain.item.ItemType;
+import hello.item_service.domain.item.SaveCheck;
+import hello.item_service.domain.item.UpdateCheck;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+	private final ItemRepository itemRepository;
+
+	@ModelAttribute("regions")
+	public Map<String, String> regions() {
+		Map<String, String> regions = new LinkedHashMap<>();
+		regions.put("SEOUL", "서울");
+		regions.put("BUSAN", "부산");
+		regions.put("JEJU", "제주");
+
+		return regions;
+	}
+
+	@ModelAttribute("itemTypes")
+	public ItemType[] itemTypes() {
+		return ItemType.values();
+	}
+
+	@ModelAttribute("deliveryCodes")
+	public List<DeliveryCode> deliveryCodes() {
+		List<DeliveryCode> deliveryCodes = new ArrayList<>();
+
+		deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+		deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+		deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+		return deliveryCodes;
+	}
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+
+		return "validation/v4/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String items(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v4/item";
+	}
+
+	@GetMapping("/add")
+	public String addForm(Model model) {
+		model.addAttribute("item", new Item());
+
+		return "validation/v4/addForm";
+	}
+
+	// @PostMapping("/add")
+	// public String addItem(@Validated @ModelAttribute Item item,
+	//                         BindingResult bindingResult,
+	//                         RedirectAttributes redirectAttributes,
+	//                         Model model) {
+	// 	// 특정 필드가 아닌 복합 룰 검증
+	// 	if (item.getPrice() != null && item.getQuantity() != null) {
+	// 		int resultPrice = item.getPrice() * item.getQuantity();
+	//
+	// 		if (resultPrice < 10000) {
+	// 			bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+	// 		}
+	// 	}
+	//
+	// 	// 검증에 실패하면 다시 입력 폼으로
+	// 	if (bindingResult.hasErrors()) {
+	// 		log.info("error = {}", bindingResult);
+	//
+	// 		return "validation/v4/addForm";
+	// 	}
+	//
+	// 	// 성공 로직
+	// 	Item savedItem = itemRepository.save(item);
+	// 	redirectAttributes.addAttribute("itemId", savedItem.getId());
+	// 	redirectAttributes.addAttribute("status", true);
+	// 	return "redirect:/validation/v4/items/{itemId}";
+	// }
+
+	@PostMapping("/add")
+	public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item,
+	                        BindingResult bindingResult,
+	                        RedirectAttributes redirectAttributes,
+	                        Model model) {
+		// 특정 필드가 아닌 복합 룰 검증
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int resultPrice = item.getPrice() * item.getQuantity();
+
+			if (resultPrice < 10000) {
+				bindingResult.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
+			}
+		}
+
+		// 검증에 실패하면 다시 입력 폼으로
+		if (bindingResult.hasErrors()) {
+			log.info("error = {}", bindingResult);
+
+			return "validation/v4/addForm";
+		}
+
+		// 성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v4/items/{itemId}";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v4/editForm";
+	}
+
+	// @PostMapping("/{itemId}/edit")
+	// public String edit(@PathVariable Long itemId,
+	//                    @Validated @ModelAttribute Item item,
+	//                    BindingResult bindingResult) {
+	// 	// 특정 필드가 아닌 복합 룰 검증
+	// 	if (item.getPrice() != null && item.getQuantity() != null) {
+	// 		int resultPrice = item.getPrice() * item.getQuantity();
+	//
+	// 		if (resultPrice < 10000) {
+	// 			bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+	// 		}
+	// 	}
+	//
+	// 	if (bindingResult.hasErrors()) {
+	// 		log.info("errors = {}", bindingResult);
+	// 		return "validation/v4/editForm";
+	// 	}
+	//
+	// 	itemRepository.update(itemId, item);
+	// 	return "redirect:/validation/v4/items/{itemId}";
+	// }
+
+	@PostMapping("/{itemId}/edit")
+	public String editV2(@PathVariable Long itemId,
+	                     @Validated(UpdateCheck.class) @ModelAttribute Item item,
+	                     BindingResult bindingResult) {
+		// 특정 필드가 아닌 복합 룰 검증
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int resultPrice = item.getPrice() * item.getQuantity();
+
+			if (resultPrice < 10000) {
+				bindingResult.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
+			}
+		}
+
+		if (bindingResult.hasErrors()) {
+			log.info("errors = {}", bindingResult);
+			return "validation/v4/editForm";
+		}
+
+		itemRepository.update(itemId, item);
+		return "redirect:/validation/v4/items/{itemId}";
+	}
+}
