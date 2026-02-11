2be78f446ed125566560e3c928ded43d89beec13
pakms980319 <pakms980319@gmail.com>
2026-02-11 10:19:03 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 9. 예외 처리와 오류 페이지 - 서블릿 예외 처리 - 필터

---
+++ 05-spring-mvc-2/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,27 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public FilterRegistrationBean logFilter() {
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+		filterRegistrationBean.setFilter(new LogFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.addUrlPatterns("/*");
+		filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+
+		return filterRegistrationBean;
+	}
+}
