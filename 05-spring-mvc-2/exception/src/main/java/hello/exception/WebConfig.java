ab114fff6b4c8b4d92b0b461a26d5bc2a2f0fadc
pakms980319 <pakms980319@gmail.com>
2026-02-11 13:56:06 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 10. API 예외 처리 - HandlerExceptionResolver 시작

--- 05-spring-mvc-2/exception/src/main/java/hello/exception/WebConfig.java
+++ 05-spring-mvc-2/exception/src/main/java/hello/exception/WebConfig.java
@@ -2,18 +2,27 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		resolvers.add(new MyHandlerExceptionResolver());
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
