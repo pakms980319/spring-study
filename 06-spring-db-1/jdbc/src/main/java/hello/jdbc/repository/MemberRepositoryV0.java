fb495bb37df5fb4965cc3b8da054f4708422849a
pakms980319 <pakms980319@gmail.com>
2026-02-23 17:04:37 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 섹션 2. JDBC 이해 - JDBC 개발 - 조회

--- 06-spring-db-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ 06-spring-db-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -9,12 +9,42 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
  */
 @Slf4j
 public class MemberRepositoryV0 {
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		}
+	}
 
 	public Member save(Member member) throws SQLException {
 		String sql = "insert into member(member_id, money) values (?, ?)";
