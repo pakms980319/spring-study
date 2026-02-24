b46208df01ef158481e7bd9fec5f97cb0867c456
pakms980319 <pakms980319@gmail.com>
2026-02-24 10:17:09 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 섹션 3. 커넥션풀과 데이터소스 이해 - 커넥션 풀 예제

--- 06-spring-db-1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ 06-spring-db-1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -8,11 +8,27 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.zaxxer.hikari.HikariDataSource;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class ConnectionTest {
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		// 커넥션 풀링
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("MyPool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);  // 커넥션 풀에서 커넥션이 생성되기를 대기.
+	}
+
 	@Test
 	void dataSourceDriverManager() throws SQLException {
 		// DriverManagerDataSource - 항상 새로운 커넥션을 획득
