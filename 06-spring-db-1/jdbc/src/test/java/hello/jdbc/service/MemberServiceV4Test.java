6dcc198b3b4e0be13b4500e23068c1b0fff8e84b
pakms980319 <pakms980319@gmail.com>
2026-02-27 15:32:59 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 섹션 7. 스프링과 문제해결 - 예외 처리 반복 - JDBC 반복 문제 해결 - JdbcTemplate

--- 06-spring-db-1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ 06-spring-db-1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV4_1;
 import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
@@ -44,7 +45,8 @@ class MemberServiceV4Test {
 		@Bean
 		MemberRepository memberRepository() {
 			// return new MemberRepositoryV4_1(dataSource);
-			return new MemberRepositoryV4_2(dataSource);
+			// return new MemberRepositoryV4_2(dataSource);
+			return new MemberRepositoryV5(dataSource);
 		}
 
 		@Bean
